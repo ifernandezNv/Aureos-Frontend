edff94b6f09402c57037e1793053248ef43dedec
ifernandeznv <ifernandeznv@gmail.com>
2022-12-12 17:43:03 -0600

    ajustes

--- components/Usuario.js
+++ components/Usuario.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Pressable, Alert } from 'react-native'
+import { View, Text, StyleSheet, Pressable, Alert } from 'react-native';
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 import useAureos from '../hooks/useAureos';
